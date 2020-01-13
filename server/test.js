b1a926feffeeb3d0256178e63aa901f4965112ef
sammy0807 <sami.egwu90@gmail.com>
2020-01-13 04:13:31 -0600

    latest bit

---
+++ server/test.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+(() => {
+  const obj = {
+    a: 1,
+    b: 2,
+    getA() {
+      console.log(this.a);
+      return this;
+    },
+    getB() {
+      console.log(this.b);
+    }
+  };
+
+  obj.getA().getB();
+})();
--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,7 @@ import store from "@/store/store";
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081`,
+    baseURL: `http://localhost:${process.env.PORT + 1} || 8081 || 50001`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
